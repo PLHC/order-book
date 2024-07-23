ab5bb6d742b4e695e28c74a1748534b74adb1ab6
Paul Chaucheprat <125810956+PLHC@users.noreply.github.com>
2024-07-23 14:33:46 +0200

    update gitignore

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/paul/CLionProjects/OrderBook
+# Install script for directory: /Users/paul/CLionProjects/OrderBook/order-book
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/paul/CLionProjects/OrderBook/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/paul/CLionProjects/OrderBook/order-book/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
